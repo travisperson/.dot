149399bd56f2c9050618d4ac21587b2c63132471
Travis Person <travis.person@gmail.com>
2018-03-12 16:21:11 -0700

    Enable setup to run from symlink, enable dhcp

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+sudo dhcpcd ens33; sleep 5
 sudo pacman -Syu
-sudo pacman -S --noconfirm - < pacman.txt
+sudo pacman -S --noconfirm - < $HOME/dot/pacman.txt
 # yaourt -S --noconfirm - < yaourt.txt
 
 rm -f /home/travis/.setup
