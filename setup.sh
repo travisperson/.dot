7ca69734103f42481781face1b7d1f7a667281cb
Travis Person <travis.person@gmail.com>
2018-03-13 21:58:39 -0700

    Yaourt install

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ iface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | tr -d 
 sudo dhcpcd $iface; sleep 5
 sudo pacman -Syu
 sudo pacman -S --noconfirm - < $HOME/dot/pacman.txt
-# yaourt -S --noconfirm - < yaourt.txt
+
+# This conflicts with i3lock-color-git in yaourt
+sudo pacman -Rns --noconfirm i3lock
+
+yaourt -S --noconfirm $(cat $HOME/dot/yaourt.txt)
 
 rm -f /home/travis/.setup
