c954e7b71aef9eeb4b77a22cb506a2faa509ca6f
Travis Person <travis.person@gmail.com>
2018-03-13 20:12:15 -0700

    Detect iface

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo dhcpcd ens33; sleep 5
+iface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | tr -d ' ')
+
+sudo dhcpcd $iface; sleep 5
 sudo pacman -Syu
 sudo pacman -S --noconfirm - < $HOME/dot/pacman.txt
 # yaourt -S --noconfirm - < yaourt.txt
