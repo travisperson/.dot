ec70b6993688231ec15ed1db88c158cf753a64e6
Travis Person <travis.person@gmail.com>
2018-03-15 21:35:04 -0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
-iface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | tr -d ' ')
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+BASE=$(pwd)
+IFACE=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | tr -d ' ')
 
 sudo dhcpcd $iface; sleep 5
 sudo pacman -Syu
-sudo pacman -S --noconfirm - < $HOME/dot/pacman.txt
+sudo pacman -S --noconfirm - < pacman.txt
 
 # This conflicts with i3lock-color-git in yaourt
 sudo pacman -Rns --noconfirm i3lock
 
-yaourt -S --noconfirm $(cat $HOME/dot/yaourt.txt)
+yaourt -S --noconfirm $(cat yaourt.txt)
 
-ln -sf $HOME/dot/vim $HOME/.vim
-ln -sf $HOME/dot/config $HOME/.config
+for rc in xinitrc Xdefaults gitconfig vim config; do
+  ln -sfv "$BASE/$rc" $HOME/."$rc"
+done
 
-mkdir .apps/go
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+mkdir $HOME/.apps/go
 
 curl -L https://dl.google.com/go/go1.10.linux-amd64.tar.gz | tar xz -C $HOME/.apps
+
+chsh -s $(which fish) $USER
 
 rm -f $HOME/.setup
