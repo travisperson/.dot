9c5376711a582cd2678d3e3c41a4dcff186c42e1
Travis Person <travis.person@gmail.com>
2018-03-30 19:52:39 -0700

    Add default workspace layout

--- i3-workspaces/ipfs-proxy.sh
+++ i3-workspaces/ipfs-proxy.sh
@@ -1,40 +1,14 @@
 #!/usr/bin/env bash
+cd "$(dirname ${BASH_SOURCE[0]})" || exit 1
+source .shared
 
-function start {
-  local args=$1
+if [ -f $HOME/.ipfs/repo.lock ]; then
+  i3-msg "[title=ipfs-daemon-watch] focus"
+  exit 1
+fi
 
-  urxvt -e fish -liC "$args" &
-  go-psleep 100ms
-}
+load_workspace $(current_workspace) $(workspace_name $BASH_SOURCE)
 
-function load_workspace {
-  local workspace=$1
-  local layout=$2
-
-  i3-msg "workspace $workspace; append_layout $HOME/.dot/i3-workspaces/$layout"
-}
-
-function toggle_split {
-  i3-msg "layout toggle split"
-}
-
-function down {
-  i3-msg "focus down"
-}
-
-function left {
-  i3-msg "focus left"
-}
-
-function up {
-  i3-msg "focus up"
-}
-
-function right {
-  i3-msg "focus right"
-}
-
-load_workspace 2 "ipfs-proxy.workspace"
 toggle_split
 
 start "ipfs-proxy proxy"
@@ -46,4 +20,3 @@ right
 start "ipfs-proxy proxy-out"
 up
 start "ipfs-proxy proxy-in"
-
---
+++ i3-workspaces/layout-three-columns.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$(dirname ${BASH_SOURCE[0]})" || exit 1
+source .shared
+
+load_workspace $(current_workspace) $(workspace_name $BASH_SOURCE)
+
+start
+start
+start
